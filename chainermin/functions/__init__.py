9f4ddd73db5032403e1dd1de2303efb3e860a264
tsurumeso <nstm101339@gmail.com>
2017-07-19 11:31:20 +0900

    Implement F.softmax_cross_entropy

--- chainermin/functions/__init__.py
+++ chainermin/functions/__init__.py
@@ -8,5 +8,6 @@ from chainermin.functions.connection.linear import linear  # NOQA
 from chainermin.functions.evaluation.accuracy import accuracy  # NOQA
 
 from chainermin.functions.loss.mean_squared_error import mean_squared_error  # NOQA
+from chainermin.functions.loss.softmax_cross_entropy import softmax_cross_entropy  # NOQA
 
 from chainermin.functions.noise.dropout import dropout  # NOQA
---
+++ chainermin/functions/loss/softmax_cross_entropy.py
@@ -0,0 +1,57 @@
+import numpy
+
+from chainer import function
+from chainer.functions.activation import log_softmax
+
+
+class SoftmaxCrossEntropy(function.Function):
+
+    def __init__(self, normalize=True, cache_score=True):
+        self.normalize = normalize
+
+    def forward(self, inputs):
+        x, t = inputs
+        log_y = log_softmax._log_softmax(x)
+        if self.cache_score:
+            self.y = numpy.exp(log_y)
+        log_yd = numpy.rollaxis(log_y, 1)
+        log_yd = log_yd.reshape(len(log_yd), -1)
+        log_p = log_yd[numpy.maximum(t.ravel(), 0), numpy.arange(t.size)]
+
+        if self.normalize:
+            count = (t != self.ignore_label).sum()
+        else:
+            count = len(x)
+        self._coeff = 1.0 / max(count, 1)
+
+        y = log_p.sum(keepdims=True) * (-self._coeff)
+        return y.reshape(()),
+
+    def backward(self, inputs, grad_outputs):
+        x, t = inputs
+        gloss = grad_outputs[0]
+        if hasattr(self, 'y'):
+            y = self.y.copy()
+        else:
+            y = log_softmax._log_softmax(x)
+            numpy.exp(y, out=y)
+
+        if y.ndim == 2:
+            gx = y
+            gx[numpy.arange(len(t)), numpy.maximum(t, 0)] -= 1
+            # gx *= (t != self.ignore_label).reshape((len(t), 1))
+        else:
+            n_unit = t.size // len(t)
+            gx = y.reshape(y.shape[0], y.shape[1], -1)
+            fst_index = numpy.arange(t.size) // n_unit
+            trd_index = numpy.arange(t.size) % n_unit
+            gx[fst_index, numpy.maximum(t.ravel(), 0), trd_index] -= 1
+            # gx *= (t != self.ignore_label).reshape((len(t), 1, -1))
+            gx = gx.reshape(y.shape)
+
+        gx *= gloss * self._coeff
+        return gx, None
+
+
+def softmax_cross_entropy(x, t, normalize=True, cache_score=True):
+    return SoftmaxCrossEntropy(normalize, cache_score)(x, t)
