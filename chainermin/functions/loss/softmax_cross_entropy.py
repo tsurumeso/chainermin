bb115d0c48fc198bd266564d16d3761d6ee5695b
tsurumeso <nstm101339@gmail.com>
2017-07-21 18:37:51 +0900

    Fix F.softmax_cross_entropy

--- chainermin/functions/loss/softmax_cross_entropy.py
+++ chainermin/functions/loss/softmax_cross_entropy.py
@@ -6,8 +6,10 @@ from chainermin.functions.activation import log_softmax
 
 class SoftmaxCrossEntropy(function.Function):
 
-    def __init__(self, normalize=True, cache_score=True):
+    def __init__(self, normalize=True, cache_score=True, ignore_label=-1):
         self.normalize = normalize
+        self.cache_score = cache_score
+        self.ignore_label = ignore_label
 
     def forward(self, inputs):
         x, t = inputs
@@ -18,6 +20,7 @@ class SoftmaxCrossEntropy(function.Function):
         log_yd = log_yd.reshape(len(log_yd), -1)
         log_p = log_yd[numpy.maximum(t.ravel(), 0), numpy.arange(t.size)]
 
+        log_p *= (t.ravel() != self.ignore_label)
         if self.normalize:
             count = (t != self.ignore_label).sum()
         else:
@@ -39,19 +42,21 @@ class SoftmaxCrossEntropy(function.Function):
         if y.ndim == 2:
             gx = y
             gx[numpy.arange(len(t)), numpy.maximum(t, 0)] -= 1
-            # gx *= (t != self.ignore_label).reshape((len(t), 1))
+            gx *= (t != self.ignore_label).reshape((len(t), 1))
         else:
             n_unit = t.size // len(t)
             gx = y.reshape(y.shape[0], y.shape[1], -1)
             fst_index = numpy.arange(t.size) // n_unit
             trd_index = numpy.arange(t.size) % n_unit
             gx[fst_index, numpy.maximum(t.ravel(), 0), trd_index] -= 1
-            # gx *= (t != self.ignore_label).reshape((len(t), 1, -1))
+            gx *= (t != self.ignore_label).reshape((len(t), 1, -1))
             gx = gx.reshape(y.shape)
 
         gx *= gloss * self._coeff
         return gx, None
 
 
-def softmax_cross_entropy(x, t, cache_score=True):
-    return SoftmaxCrossEntropy(cache_score)(x, t)
+def softmax_cross_entropy(
+        x, t, normalize=True, cache_score=True,
+        ignore_label=-1):
+    return SoftmaxCrossEntropy(normalize, cache_score, ignore_label)(x, t)
