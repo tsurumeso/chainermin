34e55d86c375236a067b6aaec9b645ea51d6b468
tsurumeso <nstm101339@gmail.com>
2017-09-19 18:34:58 +0900

    Simplify LinearFunction

--- chainermin/functions/connection/linear.py
+++ chainermin/functions/connection/linear.py
@@ -4,15 +4,14 @@ from chainermin import function
 class LinearFunction(function.Function):
 
     def forward(self, inputs):
-        x = inputs[0]
-        W = inputs[1]
-        b = inputs[2]
+        x, W, b = inputs[:3]
+
         y = x.dot(W.T) + b
         return y,
 
     def backward(self, inputs, grad_outputs):
-        x = inputs[0]
-        W = inputs[1]
+        x, W = inputs[:2]
+
         gy = grad_outputs[0]
 
         gx = gy.dot(W).reshape(x.shape)
