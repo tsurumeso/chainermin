8cdcd64a1f14097ab2a329459ac2abbda67c1843
tsurumeso <tsurumeso@gmail.com>
2018-09-27 18:12:59 +0900

    Remove blank lines

--- chainermin/functions/connection/linear.py
+++ chainermin/functions/connection/linear.py
@@ -5,14 +5,12 @@ class LinearFunction(function.Function):
 
     def forward(self, inputs):
         x, W, b = inputs[:3]
-
         y = x.dot(W.T) + b
         return y,
 
     def backward(self, inputs, grad_outputs):
         x, W = inputs[:2]
         gy = grad_outputs[0]
-
         gx = gy.dot(W).reshape(x.shape)
         gW = gy.T.dot(x)
         gb = gy.sum(axis=0)
